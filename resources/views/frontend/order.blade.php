91406b9721d44c1d53d4fe6fde51e0fae1be113a
letamhot <letamhot@gmail.com>
2020-10-17 11:41:27 +0700

    update chat facebook

--- resources/views/frontend/order.blade.php
+++ resources/views/frontend/order.blade.php
@@ -56,7 +56,32 @@
         {{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script> --}}
         <script src="js/main.js"></script>
         <script src="js/script.js"></script>
+        <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v8.0'
+        });
+      };
 
+      (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Your Chat Plugin code -->
+    <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="173337116535784"
+        theme_color="#0A7CFF"
+        logged_in_greeting="Xin chào! Shop có thể giúp bạn gì ạ!!!"
+        logged_out_greeting="Xin chào! Shop có thể giúp bạn gì ạ!!!">
+    </div>
 
 
 
